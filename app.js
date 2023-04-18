88ec23544edbed100e7bafbdc0460ccd5e686296
Gubriyel <officialgabrielsufrir@gmail.com>
2023-04-19 02:36:30 +0800

    Changed functions into arrow functions

--- app.js
+++ app.js
@@ -11,20 +11,26 @@ toggle.onclick = () => {
 
 // Mobile Menu (Remove)
 const navLink = document.querySelectorAll('.nav__link');
-function linkAction() {
+const linkAction = () => {
   nav.classList.remove('show__menu');
-}
+};
 
-navLink.forEach((e) => e.addEventListener('click', linkAction));
+navLink.forEach((e) => {
+  e.addEventListener('click', linkAction);
+});
 
 // Change Link Color
 
 const linkColor = document.querySelectorAll('.nav__link');
-function colorLink() {
+const colorLink = () => {
   if (colorLink) {
-    linkColor.forEach((e) => e.classList.remove('active'));
+    linkColor.forEach((e) => {
+      e.classList.remove('active');
+    });
     this.classList.add('active');
   }
-}
+};
 
-linkColor.forEach((e) => e.addEventListener('click', colorLink));
+linkColor.forEach((e) => {
+  e.addEventListener('click', colorLink);
+});
