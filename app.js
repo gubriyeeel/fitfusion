eda1e86efb6f8fd92b6aa722d8e2aabd5573cda6
Gubriyel <officialgabrielsufrir@gmail.com>
2023-04-19 02:07:06 +0800

    Changed some visuals and changed icons to BX to FA

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 // Mobile Menu (Show)
 const nav = document.querySelector('.nav');
 const toggle = document.querySelector('.nav__toggle');
+const burgerMenu = document.querySelector('#burger-menu');
+
 toggle.onclick = () => {
   nav.classList.toggle('show__menu');
-
-  let navClass = nav.classList;
-  console.log(navClass);
-  
-}
+  burgerMenu.classList.toggle('fa-bars');
+  burgerMenu.classList.toggle('fa-x');
+};
 
 // Mobile Menu (Remove)
 const navLink = document.querySelectorAll('.nav__link');
 function linkAction() {
-  const navMenu = document.querySelector('.nav');
   nav.classList.remove('show__menu');
 }
 
-navLink.forEach(e => e.addEventListener('click', linkAction))
+navLink.forEach((e) => e.addEventListener('click', linkAction));
 
 // Change Link Color
 
 const linkColor = document.querySelectorAll('.nav__link');
-function colorLink () {
+function colorLink() {
   if (colorLink) {
-    linkColor.forEach(e => e.classList.remove('active'));
+    linkColor.forEach((e) => e.classList.remove('active'));
     this.classList.add('active');
   }
 }
 
-linkColor.forEach(e => e.addEventListener('click', colorLink));
+linkColor.forEach((e) => e.addEventListener('click', colorLink));
