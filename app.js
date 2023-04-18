e1715774a58e0c7326c46a252fe696ddba77c006
Gubriyel <officialgabrielsufrir@gmail.com>
2023-04-19 03:31:48 +0800

    Added variables, styles, and contained the company name into a single span

--- app.js
+++ app.js
@@ -6,31 +6,25 @@ const burgerMenu = document.querySelector('#burger-menu');
 toggle.onclick = () => {
   nav.classList.toggle('show__menu');
   burgerMenu.classList.toggle('fa-bars');
-  burgerMenu.classList.toggle('fa-x');
+  burgerMenu.classList.toggle('fa-xmark');
 };
 
 // Mobile Menu (Remove)
 const navLink = document.querySelectorAll('.nav__link');
-const linkAction = () => {
+function linkAction() {
   nav.classList.remove('show__menu');
-};
+}
 
-navLink.forEach((e) => {
-  e.addEventListener('click', linkAction);
-});
+navLink.forEach((e) => e.addEventListener('click', linkAction));
 
 // Change Link Color
 
 const linkColor = document.querySelectorAll('.nav__link');
-const colorLink = () => {
+function colorLink() {
   if (colorLink) {
-    linkColor.forEach((e) => {
-      e.classList.remove('active');
-    });
+    linkColor.forEach((e) => e.classList.remove('active'));
     this.classList.add('active');
   }
-};
+}
 
-linkColor.forEach((e) => {
-  e.addEventListener('click', colorLink);
-});
+linkColor.forEach((e) => e.addEventListener('click', colorLink));
