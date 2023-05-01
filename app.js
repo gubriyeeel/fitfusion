17799ffbb87830d8e9edf687b8f72ec555a0a58d
Gubriyel <officialgabrielsufrir@gmail.com>
2023-05-01 10:31:35 +0800

    Added some changes on the styles and added typeof check on JS

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-// Mobile Menu (Show)
-const nav = document.querySelector('.nav');
-const toggle = document.querySelector('.nav__toggle');
-const burgerMenu = document.querySelector('#burger__menu');
-const backgroundSlider = document.querySelector('#sliding__background');
+if (typeof window !== 'undefined') {
+  // Mobile Menu (Show)
+  const nav = document.querySelector('.nav');
+  const toggle = document.querySelector('.nav__toggle');
+  const burgerMenu = document.querySelector('#burger__menu');
+  const backgroundSlider = document.querySelector('#sliding__background');
 
-const backgroundImages = [
-  '../assets/images/background-slide-one.jpg',
-  '../assets/images/background-slide-three.jpg',
-  '../assets/images/background-slide-two.jpg',
-];
+  const backgroundImages = [
+    '../assets/images/background-slide-one.jpg',
+    '../assets/images/background-slide-three.jpg',
+    '../assets/images/background-slide-two.jpg',
+  ];
 
-const numBackgroundImages = backgroundImages.length;
-let currentBackgroundIndex = 0;
-const nextBackgroundImage = new Image();
+  const numBackgroundImages = backgroundImages.length;
+  let currentBackgroundIndex = 0;
+  const nextBackgroundImage = new Image();
 
-const nextBackground = () => {
-  currentBackgroundIndex = (currentBackgroundIndex + 1) % numBackgroundImages;
-  backgroundSlider.style.opacity = 0;
-  nextBackgroundImage.onload = () => {
-    backgroundSlider.style.backgroundImage = `url(${nextBackgroundImage.src})`;
-    backgroundSlider.style.opacity = 1;
+  const nextBackground = () => {
+    currentBackgroundIndex = (currentBackgroundIndex + 1) % numBackgroundImages;
+    backgroundSlider.style.opacity = 0;
+    nextBackgroundImage.onload = () => {
+      backgroundSlider.style.backgroundImage = `url(${nextBackgroundImage.src})`;
+      backgroundSlider.style.opacity = 1;
+    };
+    nextBackgroundImage.src = backgroundImages[currentBackgroundIndex];
   };
-  nextBackgroundImage.src = backgroundImages[currentBackgroundIndex];
-};
 
-nextBackground();
-setInterval(nextBackground, 4000);
+  nextBackground();
+  setInterval(nextBackground, 4000);
 
-toggle.onclick = () => {
-  nav.classList.toggle('show__menu');
-  burgerMenu.classList.toggle('fa-bars');
-  burgerMenu.classList.toggle('fa-xmark');
-};
+  toggle.onclick = () => {
+    nav.classList.toggle('show__menu');
+    burgerMenu.classList.toggle('fa-bars');
+    burgerMenu.classList.toggle('fa-xmark');
+  };
 
-// Mobile Menu (Remove)
-const navLink = document.querySelectorAll('.nav__link');
-function linkAction() {
-  nav.classList.remove('show__menu');
-}
-
-navLink.forEach((e) => e.addEventListener('click', linkAction));
-
-// Change Link Color
-
-const linkColor = document.querySelectorAll('.nav__link');
-function colorLink() {
-  if (colorLink) {
-    linkColor.forEach((e) => e.classList.remove('active'));
-    this.classList.add('active');
+  // Mobile Menu (Remove)
+  const navLink = document.querySelectorAll('.nav__link');
+  function linkAction() {
+    nav.classList.remove('show__menu');
   }
-}
 
-linkColor.forEach((e) => e.addEventListener('click', colorLink));
+  navLink.forEach((e) => e.addEventListener('click', linkAction));
+
+  // Change Link Color
+
+  const linkColor = document.querySelectorAll('.nav__link');
+  function colorLink() {
+    if (colorLink) {
+      linkColor.forEach((e) => e.classList.remove('active'));
+      this.classList.add('active');
+    }
+  }
+
+  linkColor.forEach((e) => e.addEventListener('click', colorLink));
+}
